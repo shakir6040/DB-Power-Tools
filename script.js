4429748014ef391d6dcd3d67f969c19dd192ddfb
shakir6040 <77540454+shakir6040@users.noreply.github.com>
2023-04-14 21:13:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper"),
+  qrInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value;
+  if (!qrValue || preValue === qrValue) return;
+  preValue = qrValue;
+  generateBtn.innerText = "Generating QR Code...";
+  let upiPay = `upi://pay?pa=9848952217@ybl&am=${qrValue}&pn=jahangir%20md`;
+  qrImg.src = `https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=250x250&chl=${encodeURIComponent(upiPay)}`;
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code";
+  });
+});
+qrInput.addEventListener("keyup", () => {
+  if (!qrInput.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+  }
+});
